e16432d215937552f16e3888c44cb49c91665aa8
Vassilix12 <Vassili.Kafentzis@gmail.com>
2025-05-12 18:52:40 -0400

    Finish Firebase GitHub deploy setup

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'quiziq',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'quiziq',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
